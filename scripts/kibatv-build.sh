3c11e3fd7fddda77ecaa5dc3b9913f9657f78e22
Christopher L Fox Junior <wolftechinnovationsgithub@gmail.com>
2026-05-04 11:47:26 -0500

    Update kibatv-build.sh

--- scripts/kibatv-build.sh
+++ scripts/kibatv-build.sh
@@ -40,7 +40,9 @@ echo "deb [signed-by=/usr/share/keyrings/debian-archive-keyring.gpg] \
 apt-get update -o Dir::Etc::sourcelist="sources.list.d/debian-trixie.list" \
                -o Dir::Etc::sourceparts="-" \
                -o APT::Get::List-Cleanup="0"
-
+for dir in /lib32 /lib64 /libx32 /sbin /bin /lib; do
+  dpkg-divert --remove "$dir" 2>/dev/null || true
+done
 apt-get install -y -t trixie live-build
 
 
