8f85912caed53f044f6af33a714bb81df93981a9
Christopher L Fox Junior <wolftechinnovationsgithub@gmail.com>
2026-05-04 07:13:09 -0500

    Update kibatv-build.sh

--- scripts/kibatv-build.sh
+++ scripts/kibatv-build.sh
@@ -254,7 +254,8 @@ apt-get install -y --no-install-recommends \
   'qt6-base-dev-tools=6.8.2+dfsg-9+deb13u1'
 git clone https://invent.kde.org/frameworks/extra-cmake-modules.git
 cd extra-cmake-modules
-sed -i 's/find_package(KF6 [0-9.]* REQUIRED/find_package(KF6 REQUIRED/g' CMakeLists.txt
+sed -i 's/find_package(KF6 6.14.0 REQUIRED/find_package(KF6 REQUIRED/g' CMakeLists.txt
+sed -i -E 's/([0-9]+)\.14(\.[0-9]+)?/\113\2/g' CMakeLists.txt
 git checkout v6.14.0
 cmake -B build
 cmake --install build
@@ -264,7 +265,8 @@ git clone --depth=1 https://invent.kde.org/plasma/plasma-bigscreen.git 2>/dev/nu
   git clone --depth=1 https://github.com/KDE/plasma-bigscreen.git
 
 cd plasma-bigscreen
-sed -i 's/find_package(KF6 [0-9.]* REQUIRED/find_package(KF6 REQUIRED/g' CMakeLists.txt
+sed -i 's/find_package(KF6 6.14.0 REQUIRED/find_package(KF6 REQUIRED/g' CMakeLists.txt
+sed -i -E 's/([0-9]+)\.14(\.[0-9]+)?/\113\2/g' CMakeLists.txt
 mkdir build && cd build
 cmake .. \
   -DCMAKE_INSTALL_PREFIX=/usr \
