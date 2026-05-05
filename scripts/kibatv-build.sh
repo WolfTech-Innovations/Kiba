3d0e7c16f16e882bf634eb8f0bcb9a0b56f6442d
Christopher L Fox Junior <wolftechinnovationsgithub@gmail.com>
2026-05-05 09:07:18 -0500

    Update kibatv-build.sh

--- scripts/kibatv-build.sh
+++ scripts/kibatv-build.sh
@@ -469,7 +469,7 @@ pmbootstrap --as-root config sudo_timer False
 pmbootstrap --as-root config extra_packages none
 pmbootstrap --as-root build kibatv-config
 pmbootstrap --as-root build kstore
-pmbootstrap --as-root install --no-fde --add kibatv-config,kstore
+pmbootstrap --as-root --details-to-stdout --no-sshd --no-firewall --no-fde install --add kibatv-config,kstore
 
 RAW_IMG=$(find /work/pmb/export -name "*.img" | head -1)
 
