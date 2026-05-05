08463ad5b4e89d36af18ab6ea55a5d592a489ffa
Christopher L Fox Junior <wolftechinnovationsgithub@gmail.com>
2026-05-05 08:39:13 -0500

    Update kibatv-build.sh

--- scripts/kibatv-build.sh
+++ scripts/kibatv-build.sh
@@ -613,7 +613,7 @@ pmbootstrap --as-root config work $WORKDIR
 pmbootstrap --as-root config device qemu-amd64
 pmbootstrap --as-root config kernel stable
 pmbootstrap --as-root config ui plasma-bigscreen
-pmbootstrap --as-root config username user
+pmbootstrap --as-root config user user
 pmbootstrap --as-root config timezone UTC
 pmbootstrap --as-root config locale en_US
 pmbootstrap --as-root config hostname kibatv
