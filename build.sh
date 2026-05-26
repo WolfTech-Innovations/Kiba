3b2dbdc52c3c6d7bc795086c75f9157b5ec89f33
Christopher L Fox Junior <wolftechinnovationsgithub@gmail.com>
2026-05-25 21:39:56 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1308,7 +1308,18 @@ cat > /usr/share/kibaos/welcome.html << 'WELCOMEHTML'
 </body>
 </html>
 WELCOMEHTML
+# Add Chaotic-AUR
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U --noconfirm \
+  'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' \
+  'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
+echo '[chaotic-aur]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
+
+pacman -Syy --noconfirm
+pacman -S --noconfirm calamares
 # ══════════════════════════════════════════════════════════════════════════
 # SYSTEM BRANDING
 # ══════════════════════════════════════════════════════════════════════════
