0c0ed80d40c529cf58875b2b89fad2badfe5130f
Remi Mixo <wolftechinnovationsgithub@gmail.com>
2026-09-07 07:22:53 -0500

    Add 'icons' and 'cutefish-framework' to repo cloning

--- build.sh
+++ build.sh
@@ -6099,7 +6099,7 @@ QTPATHS6
   chmod +x /usr/local/bin/qtpaths6
   echo "=== qtpaths6 shim installed (wraps qmake6 -query) ==="
 
-  for _repo in fishui filemanager settings core shell; do
+  for _repo in fishui filemanager settings core icons cutefish-framework shell; do
     git clone --depth 1 "https://github.com/cutefishos/${_repo}.git" \
       "${CUTEFISH_SRC}/${_repo}"
     cmake -S "${CUTEFISH_SRC}/${_repo}" -B "${CUTEFISH_SRC}/${_repo}/build" \
