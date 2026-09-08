26e34155be9ac8e613a03bd09b502d600eedda6c
Remi Mixo <wolftechinnovationsgithub@gmail.com>
2026-09-08 18:25:46 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -10937,7 +10937,7 @@ mkdir -p /etc/ddm.conf.d
 cat > /etc/ddm.conf.d/kibaos-oem-autologin.conf << 'OEMAUTOLOGIN'
 [Autologin]
 User=oem
-Session=treeland.desktop
+Session=budgie.desktop
 OEMAUTOLOGIN
 
 # OOBE app autostarts for the oem user too, in OEM-finish mode (the
@@ -12014,7 +12014,7 @@ mkdir -p /etc/ddm.conf.d
 cat > /etc/ddm.conf.d/kibaos.conf << 'SDDMCONF'
 [Autologin]
 User=liveuser
-Session=treeland.desktop
+Session=budgie.desktop
 SDDMCONF
 
 mkdir -p /var/lib/ddm
