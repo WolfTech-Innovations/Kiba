8dd4456a30deb55993a7fa9cd1aa3a67f6818a62
Remi Mixo <wolftechinnovationsgithub@gmail.com>
2026-09-11 15:05:55 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3493,7 +3493,7 @@ mkdir -p build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . -j"$(nproc)"
 pacman -Syu --noconfirm gtk4 libadwaita vala meson ninja
-pacman -Syu --noconfirm extra-cmake-modules qt5-base qt5-quickcontrols2 freetype2 fontconfig networkmanager-qt modemmanager-qt kcoreaddons
+pacman -Syu --noconfirm extra-cmake-modules qt5-base qt5-quickcontrols2 freetype2 fontconfig networkmanager-qt modemmanager-qt kcoreaddons qt5-tools qt6-tools
 git clone https://github.com/felixonmars/settings
 cd settings
 mkdir build
